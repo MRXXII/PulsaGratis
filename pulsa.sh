c074216d209a0e7a4d16b4a6f657915b9bbb0f34
MR.XXII <betazona1984@gmail.com>
2023-09-15 01:21:53 +0900

    pulsa.sh

---
+++ pulsa.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+#Hacker Pulsa Gratis
+#Tools Ini Cuma Untuk Prank
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo
+echo  $green
+sleep 3
+clear
+echo
+echo
+echo
+toilet -f future "HACK PULSA GRATIS" | lolcat
+echo
+echo $red"##########################$green" 7F77CYBER "$red###########################"
+echo $yellow
+figlet -f future "7F77 CYBER INDONESIA"
+echo
+echo $red"##########################$green" 7F77CYBER "$red###########################"
+sleep 2
+echo
+echo "\033[0;32m]Masukan Nomer HP Anda: "
+read lu
+echo "\033[0;32m]Masukan Nomer HP Korban: "
+read korban
+echo "\033[0;32m]Jumlah Pulsa : "
+read pulsa
+echo
+echo
+echo "Anda Tunggu Sebentar !! Sekitar 3 menit.."
+echo ""
+sleep 5
+echo
+echo  $green
+echo "Loading.............";
+sleep 3
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Loading.............";
+sleep 3
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Loading.............";
+sleep 3
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo  $green
+echo "Sedang Proses Hacking........";
+sleep 5
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo
+echo "$korban, System gagal $pulsa"
+echo ""
+sleep 4
+echo "Mohon Maaf System Jaringan Tidak Stabil"
+echo ""
+sleep 3
+echo "Silahkan Mencoba Kembali Terima Kasih"
+echo
+echo "Tekan Ctrl c untuk keluar"
